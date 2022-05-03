ce52e83852f01090718e223518024e0a55f8a1ee
Willian Rébulla <willian.po@hotmail.com>
2022-05-03 00:18:33 +0000

    1630 ok

--- 1630.py
+++ 1630.py
@@ -4,12 +4,19 @@ def mdc(a, b):
     resto = a % b
     a = b
     b = resto
-  
   return a
 
-N = int(input())
-
-for x in range(N):
-    A, B = input().split(' ')
-    A, B = int(A), int(B)
-    print(mdc(A, B))
+while True:
+  try:
+    xs,ys = input().split()
+    x = int(xs)
+    y = int(ys)
+    # Em um terreno quadrado precisa só de 4 estacas
+    if x == y:
+      print(4)
+    else:
+      # Caso não seja um quadrado, divide as estacas com um intervalo igual ao mdc entre x e y.
+      divisorcomum = mdc (x, y)
+      print ('%d' % (2 * ( x/ divisorcomum) + 2 * (y / divisorcomum)))
+  except EOFError :
+    break
